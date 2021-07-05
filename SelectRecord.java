c1d762a4d554cdcbdf5d221d58a1ddf593195f08
Sayma-Haider <saimahaider965@gmail.com>
2021-07-05 16:55:52 +0600

    Update SelectRecord.java

--- SelectRecord.java
+++ SelectRecord.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class SelectRecord {
     static final String DB_URL = "jdbc:mysql://localhost:3306/STUDENTS";
     static final String USER = "Saima";
-    static final String PASS = "12345";
+    static final String PASS = "*****";
     
     public static void main(String[] args) throws SQLException {
         Connection myConn = null;
